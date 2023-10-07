c2e63ec736b330b91a7f3564f4b934672502f1ad
Josep-11281 <132529211+Josep-11281@users.noreply.github.com>
2023-10-06 19:21:25 -0600

    Componentes

--- src/Componentes/Inicio.js
+++ src/Componentes/Inicio.js
@@ -10,9 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
+        
         >
           Learn React
         </a>
